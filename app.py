369abe601189bca9113fecc8d1ac47b436f8df06
JoaoA <joaootsugua@gmail.com>
2022-08-21 18:11:13 -0300

    Deletar teste heroku

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ def buscarEPI(ca):
     epi = EPI(ca)
     return json.jsonify(epi.receberEPI())
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
