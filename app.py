e4b05e3712b293a85b778cbc6eeec3354d47c83c
JoaoAugustoMV <joaootusugua@gmail.com>
2022-09-08 12:54:35 -0300

    Add classe teste

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import re
 from EPI import  EPI
+from api_ca3 import EPI2
 from flask import Flask, json
 
 app = Flask(__name__)
@@ -14,6 +15,12 @@ def buscarEPI(ca):
     print(ca)
     epi = EPI(ca)
     return json.jsonify(epi.receberEPI())
+
+@app.route('/api/ca2/<ca>')
+def buscarEPI2(ca):
+    print(ca)
+    epi = EPI2(ca)
+    return json.jsonify(epi.retornarJSON())
 
 # if __name__ == "__main__":
 #     port = int(os.environ.get("PORT", 5000))
